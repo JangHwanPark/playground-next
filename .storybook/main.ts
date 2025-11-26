ffcf8aae8b12596477441e1c55b6cf2defd4fc0b
JangHwanPark <wkd5878@gmail.com>
2025-11-26 15:32:32 +0900

    chore(storybook): 스토리북 mdx 파일 설정 및 추가

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,21 +1,17 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 import path from 'path';
-// import webpack from 'webpack';
-// pnpm install webpack @types/webpack --save-dev
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../src/stories/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-onboarding',
-    '@storybook/addon-interactions',
+    "@storybook/addon-docs",
+    "@storybook/addon-a11y",
+    "@storybook/addon-vitest"
   ],
   framework: {
     name: '@storybook/nextjs',
     options: {},
   },
-  docs: {},
 
   // webpackFinal 설정 추가
   webpackFinal: async (config) => {
@@ -26,19 +22,6 @@ const config: StorybookConfig = {
         '@': path.resolve(process.cwd(), 'src'),
       };
     }
-
-    // DefinePlugin을 사용하여 환경 변수 Mocking 추가
-    /*if (!config.plugins) {
-      config.plugins = [];
-    }
-
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        // Supabase 클라이언트 초기화 오류를 방지하기 위해 임시 값 주입
-        'process.env.NEXT_PUBLIC_SUPABASE_URL': JSON.stringify('mock-url-for-storybook'),
-        'process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY': JSON.stringify('mock-key-for-storybook'),
-      })
-    );*/
 
     return config;
   },
